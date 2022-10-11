a2a37d5ac35a458bdfabb193129de50f2fb5bea9
NICOLETA-ANELISE BERCEA <nicoleta.bercea@stud.ubbcluj.ro>
2022-10-11 19:56:14 +0200

    Modificare exercitiu

---
+++ Controllers/Home1Controller.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Bercea_Anelise_Lab1.Controllers
+{
+    public class Home1Controller : Controller
+    {
+       
+
+        public string Index()
+        {
+            return "WELCOME!";
+        }
+
+        public string Hello()
+        {
+            return "Hello world!";
+        }
+
+        public string Person(string name, int age)
+        {
+            return name + " your age is: " + age;
+        }
+    }
+}
